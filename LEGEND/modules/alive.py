a6555cf76780c361d6ab4cefdbefeb29e83d21ab
Swami-482 <81844065+Swami-482@users.noreply.github.com>
2021-04-03 10:44:47 +0530

    Update alive.py

--- LEGEND/modules/alive.py
+++ LEGEND/modules/alive.py
@@ -15,18 +15,18 @@ import re, os
 from LEGEND.events import register
 from LEGENDX import xbot as tbot
 from LEGENDX import xbot as tgbot
-PHOTO = "https://telegra.ph/file/ea47d6949c36e59d9f697.jpg"
+PHOTO = "https://telegra.ph/file/136c61a6d5da4e46176ba.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   legendx = event.sender.first_name
-  LEGENDX = "➥ KIRITO HERE!! \n\n"
+  LEGENDX = "➥ MEWTWO HERE!! \n\n"
   LEGENDX += "➥ALL SYSTEM WORKING PROPERLY\n\n"
-  LEGENDX += "➥KIRITO OS : 3.8 LATEST\n\n"
+  LEGENDX += "➥MEWTWO OS : 3.0 LATEST\n\n"
   LEGENDX += f"➥USER: {legendx} ☺️\n\n"
   LEGENDX += "➥FULLY UPDATED\n\n"
   LEGENDX += "➥TELETHON : 1.19.5 [LATEST]\n\n"
-  LEGENDX += "!THANKS FOR ADD ME HERE!"
-  BUTTON = [[Button.url("MASTER", "https://t.me/Ken_kenaki"), Button.url("DEVLOPER", "https://t.me/Kaneki_alt")]]
+  LEGENDX += "!THANKS FOR ADDING ME HERE!"
+  BUTTON = [[Button.url("MASTER", "https://t.me/Swami_2_0_0_5"), Button.url("DEVLOPER", "https://t.me/Swami_alt")]]
   BUTTON += [[custom.Button.inline("✧REPOSITORYS✧", data="LEGENDX")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=LEGENDX,  buttons=BUTTON)
 
@@ -36,9 +36,9 @@ async def awake(event):
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"LEGENDX")))
 async def callback_query_handler(event):
 # inline by LEGENDX22 and PROBOYX🔥
-  PROBOYX = [[Button.url("REPO-KIRITO", "https://github.com/kanekiken44/KIRITO")]]
-  PROBOYX +=[[Button.url("DEPLOY-KIRITO", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Flegendxop%2Flegend-bot&template=https%3A%2F%2Fgithub.com%2FLEGENDXOP%2FLEGEND-BOTP%2FLE")]]
-  PROBOYX +=[[Button.url("SUPPORT CHANNEL", "https://t.me/kiritosupport"), Button.url("SUPPORT GROUP", "https://t.me/kiritosupport1")]]
+  PROBOYX = [[Button.url("REPO-3.0", "https://github.com/Swami-482/Mewtwo-3.0")]]
+  PROBOYX +=[[Button.url("DEPLOY-3.0", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FSwami-482%2FMewtwo-3.0&template=https%3A%2F%2Fgithub.com%2FSwami-482%2FMewtwo-3.0P%2FLE")]]
+  PROBOYX +=[[Button.url("SUPPORT CHANNEL", "https://t.me/mewtwobotsupport"), Button.url("SUPPORT GROUP", "https://t.me/mewtwo1_botsupport")]]
   PROBOYX +=[[custom.Button.inline("ALIVE", data="PROBOY")]]
   await event.edit(text=f"ALL DETAILS OF REPOS", buttons=PROBOYX)
 
@@ -48,21 +48,21 @@ async def callback_query_handler(event):
   global PHOTO
   legendx = event.sender.first_name
 # inline by LEGENDX22 and PROBOYX 🔥
-  LEGENDX = "➥ KIRITO HERE!!  \n\n"
+  LEGENDX = "➥ MEWTWO HERE!!  \n\n"
   LEGENDX += "➥ALL SYSTEM WORKING PROPERLY\n\n"
   LEGENDX += "➥USER OS : 3.8 LATEST\n\n"
   LEGENDX += f"➥USER: {legendx} ☺️\n\n"
   LEGENDX += "➥FULLY UPDATED BOT\n\n"
   LEGENDX += "➥TELETHON : 1.19.5 [LATEST]\n\n"
-  LEGENDX += "!!THANKS FOR ADD ME HERE!!"
-  BUTTONS = [[Button.url("MASTER", "https://t.me/Ken_kenaki"), Button.url("DEVLOPER", "https://t.me/Kaneki_alt")]]
+  LEGENDX += "!!THANKS FOR ADDING ME HERE!!"
+  BUTTONS = [[Button.url("MASTER", "https://t.me/Swami_2_0_0_5"), Button.url("DEVLOPER", "https://t.me/Swami_alt")]]
   BUTTONS += [[custom.Button.inline("✧REPOSITORYS✧", data="LEGENDX")]]
   await event.edit(text=LEGENDX, buttons=BUTTONS)
 
 
 @register(pattern=("/repo|/REPO"))
 async def repo(event):
-  await tbot.send_message(event.chat, "REPO OF KIRITO", buttons=[[Button.url("🛡REPO🛡", "https://github.com/kanekiken44/KIRITO")]])
+  await tbot.send_message(event.chat, "REPO OF KIRITO", buttons=[[Button.url("😼REPO😼", "https://github.com/Swami-482/Mewtwo-3.0")]])
 # PROBOYX 🔥 LEGENDX22
 
 __help__ = """
